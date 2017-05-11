fbda95d2c30059787112842c2b54400aa03f8ccb
haakotsm <hakon.smoervik@live.no>
2017-05-11 11:09:21 +0200

    la til bootstrap i composer og link til js og css

--- src/client/index.php
+++ src/client/index.php
@@ -1,16 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hakon
- * Date: 07.05.2017
- * Time: 18.04
- */
-include_once( './src/client/app/shared/database/database.php' );
-$db = new Database();
-$result = $db->select("select * from `ski_vm`.`person`");
-if(!$result) {
-    echo 'Ingen resultater';
-}
-while ($row = $result->fetch_row()) {
-    echo join(' ', $row);
-}
+	/**
+	 * Created by PhpStorm.
+	 * User: hakon
+	 * Date: 07.05.2017
+	 * Time: 18.04
+	 */
+	use Database\Database;
+
+	$db = new Database();
+	$result = $db->select( "SELECT * FROM `ski_vm`.`person`" );
+	if ( !$result ) {
+		echo 'Ingen resultater';
+	}
+	while ( $row = $result->fetch_row() ) {
+		echo join( ' ', $row );
+	}
